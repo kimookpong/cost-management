bacb167f9d4286fc2e217bed4461fa26d7792ed5
Hakim Mudor <78400565+kimookpong@users.noreply.github.com>
2025-06-09 14:36:00 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   images: {
     domains: ["hrms.wu.ac.th"],
   },
+  assetPrefix: isProd ? "/cost-management/" : "",
+  basePath: isProd ? "/cost-management" : "",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
